1b4b90b26a8ae5ef253156ef29796bd775207179
MKouyate <58953262+MKouyate@users.noreply.github.com>
2022-04-06 22:23:48 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function searchButton(){
+	var input=document.getElementById("search").value;
+	window.location.href=("https://www.google.com/search?q="+input+"&sxsrf=AOaemvKIDJkxQCIwpSMJRDReIU9Lxe2zeA%3A1634426930608&source=hp&ei=MmBrYeOmIvmawbkP8YmO4AM&iflsig=ALs-wAMAAAAAYWtuQrDIN74RvmHXCqJ4M8XznkQ7qPJz&ved=0ahUKEwjjh-7kitDzAhV5TTABHfGEAzwQ4dUDCAk&uact=5&oq="+input+"&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBQgAEJECMg4ILhCABBCxAxDHARDRAzIOCC4QgAQQsQMQxwEQ0QMyCAgAEIAEELEDMggIABCABBCxAzIICC4QgAQQsQMyCAgAEIAEELEDMg4ILhCABBCxAxDHARDRAzILCC4QgAQQxwEQrwE6BwgjEOoCECc6DQguEMcBENEDEOoCECc6DgguEIAEELEDEMcBEKMCOggILhCxAxCDAToLCAAQgAQQsQMQgwE6CAgAELEDEIMBOgQIABBDOgoILhDHARDRAxBDOg0ILhCxAxDHARDRAxBDOgQILhBDOgcILhCxAxBDOgcIABCxAxBDUMIGWMoMYKkQaAFwAHgAgAG9A4gBpw2SAQkwLjEuMy4xLjGYAQCgAQGwAQo&sclient=gws-wiz");
+	
+}
+//mew feature idea use the random thingy to make a im feeling ... everytime the user hovers over the im feeling ... buttom 
